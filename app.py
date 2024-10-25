daef0a4a92b26f6f6d1068369791d0900ea24637
Amanda Souza <amandasilvadsz@gmail.com>
2024-10-25 13:19:23 -0300

    style: removing white space and adjusting chart layout

--- app.py
+++ app.py
@@ -5,28 +5,24 @@ import folium
 from folium.plugins import HeatMap
 from streamlit_folium import st_folium
 
-# Sidebar para navegação entre páginas
+#Sidebar
 st.sidebar.title("Navegação")
 page = st.sidebar.radio("Selecione a página:", ["Página 1 - Resumo Geral", "Página 2 - Análise Comparativa"])
 
-# Sidebar para upload dos arquivos
 st.sidebar.title("Upload de Arquivos")
 uploaded_file_2023 = st.sidebar.file_uploader("Carregar dados de terremotos de 2023", type=["csv"])
 uploaded_file_2024 = st.sidebar.file_uploader("Carregar dados de terremotos de 2024", type=["csv"])
 
-# Inicializa as variáveis de dados
+# Dados
 earthquakes_2023 = None
 earthquakes_2024 = None
 
-# Carregar os dados quando os arquivos forem enviados
 if uploaded_file_2023 is not None:
     earthquakes_2023 = pd.read_csv(uploaded_file_2023)
 if uploaded_file_2024 is not None:
     earthquakes_2024 = pd.read_csv(uploaded_file_2024)
 
-
-
-# Função para exibir gráficos
+#Funções
 def plot_histogram(data):
     fig, ax = plt.subplots()
     ax.hist(data['magnitude'], bins=30, alpha=0.7, color='blue')
@@ -50,13 +46,8 @@ def plot_heatmap(data):
     st_folium(m, width=700)
 
 def plot_monthly_average_magnitude(data):
-    # Convertendo para formato de data e extraindo o mês
     data['month'] = pd.to_datetime(data['date']).dt.month
-
-    # Calculando a média de magnitude por mês
     monthly_avg_magnitude = data.groupby('month')['magnitude'].mean()
-
-    # Gráfico de barras para a média de magnitude mensal
     fig, ax = plt.subplots()
     monthly_avg_magnitude.plot(kind='bar', color='skyblue', ax=ax)
     ax.set_title('Média de Magnitude dos Terremotos por Mês')
@@ -70,7 +61,7 @@ def plot_monthly_average_magnitude(data):
 if page == "Página 1 - Resumo Geral":
     st.title("Resumo Geral dos Terremotos")
 
-    # Combina os dados de 2023 e 2024
+    # Combinação dos dados de 2023 e 2024
     combined_data = pd.DataFrame()
 
     if earthquakes_2023 is not None:
@@ -81,7 +72,7 @@ if page == "Página 1 - Resumo Geral":
         earthquakes_2024['year'] = 2024  # Adiciona coluna do ano
         combined_data = pd.concat([combined_data, earthquakes_2024], ignore_index=True)
 
-    # Filtrando dados com base nos filtros da sidebar
+    # Filtros dos dados
     if not combined_data.empty:
 
         combined_data['date'] = pd.to_datetime(combined_data.get('date', combined_data.get('time')), errors='coerce', utc=True)
@@ -107,17 +98,22 @@ if page == "Página 1 - Resumo Geral":
         st.subheader("Tabela de Detalhes dos Terremotos")
         st.dataframe(filtered_data[['date', 'magnitude', 'latitude', 'longitude', 'depth', 'place', 'tsunami', 'alert', 'year']])
 
+        #Total
         total_filtered_earthquakes = len(filtered_data)
         st.markdown(f"<h2>Total de Terremotos Filtrados: {total_filtered_earthquakes}</h2>", unsafe_allow_html=True)
 
+        #Gráfico 1
         plot_monthly_average_magnitude(filtered_data)
 
+        #Gráfico 2
         st.subheader("Histograma de Magnitude")
         plot_histogram(filtered_data)
 
+        #Gráfico 3
         st.subheader("Gráfico de Dispersão (Magnitude vs Profundidade)")
         plot_scatter(filtered_data)
 
+        #Mapa
         st.subheader("Mapa de Calor de Localização de Terremotos")
         plot_heatmap(filtered_data)
 
@@ -125,11 +121,11 @@ if page == "Página 1 - Resumo Geral":
 if page == "Página 2 - Análise Comparativa":
     st.title("Análise Comparativa entre 2023 e 2024")
 
+    #Filtro de profundidade
     if earthquakes_2023 is not None and earthquakes_2024 is not None:
 
         min_depth = st.slider("Profundidade Mínima (km):", min_value=0.0, max_value=700.0, value=0.0)
         max_depth = st.slider("Profundidade Máxima (km):", min_value=0.0, max_value=700.0, value=700.0)
-
 
         filtered_depth_2023 = earthquakes_2023[
             (earthquakes_2023['depth'] >= min_depth) &
@@ -140,7 +136,7 @@ if page == "Página 2 - Análise Comparativa":
             (earthquakes_2024['depth'] <= max_depth)
             ]
 
-        # Filtrando por ano
+        # Filtro do ano
         filtered_depth_2023 = filtered_depth_2023[pd.to_datetime(filtered_depth_2023['time'], errors='coerce').dt.year == 2023]
         filtered_depth_2024 = filtered_depth_2024[pd.to_datetime(filtered_depth_2024['date'], errors='coerce').dt.year == 2024]
 
@@ -149,9 +145,6 @@ if page == "Página 2 - Análise Comparativa":
 
         filtered_depth_2023 = filtered_depth_2023.dropna(subset=['month'])
         filtered_depth_2024 = filtered_depth_2024.dropna(subset=['month'])
-
-        total_earthquakes = len(filtered_depth_2023) + len(filtered_depth_2024)
-        st.markdown(f"<h2>Total de Terremotos Registrados (Filtrados por Profundidade): {total_earthquakes}</h2>", unsafe_allow_html=True)
 
         monthly_counts_2023 = filtered_depth_2023['month'].value_counts().sort_index()
         monthly_counts_2024 = filtered_depth_2024['month'].value_counts().sort_index()
@@ -163,7 +156,7 @@ if page == "Página 2 - Análise Comparativa":
 
         all_months_str = [str(month) for month in all_months]
 
-        # Tabela de detalhes filtrados
+        # Tabelas
         st.subheader("Tabela de Detalhes dos Terremotos Filtrados por Profundidade")
         st.write("Dados de 2023:")
         st.dataframe(filtered_depth_2023[['time', 'mag', 'latitude', 'longitude', 'depth', 'place']])
@@ -171,15 +164,16 @@ if page == "Página 2 - Análise Comparativa":
         st.write("Dados de 2024:")
         st.dataframe(filtered_depth_2024[['date', 'magnitude', 'latitude', 'longitude', 'depth', 'place']])
 
-        # Gráfico de linhas para total de terremotos por mês
+        # Total
+        total_earthquakes = len(filtered_depth_2023) + len(filtered_depth_2024)
+        st.markdown(f"<h2>Total de Terremotos Registrados (Filtrados por Profundidade): {total_earthquakes}</h2>", unsafe_allow_html=True)
+
+        # Gráfico 1
         fig, ax = plt.subplots()
         ax.plot(all_months_str, counts_2023, label='2023', marker='o', color='orange', linestyle='-', linewidth=2)
         ax.plot(all_months_str, counts_2024, label='2024', marker='o', color='green', linestyle='-', linewidth=2)
-
-        # Adicionando a área entre as linhas para facilitar a visualização
         ax.fill_between(all_months_str, counts_2023, color='orange', alpha=0.3)
         ax.fill_between(all_months_str, counts_2024, color='green', alpha=0.3)
-
         ax.set_title('Total de Terremotos por Mês (Filtrado por Profundidade)')
         ax.set_xlabel('Mês')
         ax.set_ylabel('Número de Terremotos')
@@ -188,31 +182,31 @@ if page == "Página 2 - Análise Comparativa":
         ax.set_xticklabels(all_months_str, rotation=45)
         st.pyplot(fig)
 
-
-        # Gráfico de barras de média de magnitude por ano
-        avg_mag_2023 = filtered_depth_2023['mag'].mean()  # Verifique se o nome da coluna é 'mag'
-        avg_mag_2024 = filtered_depth_2024['magnitude'].mean()  # Verifique se o nome da coluna é 'magnitude'
-
+        # Gráfico 2
+        avg_mag_2023 = filtered_depth_2023['mag'].mean()
+        avg_mag_2024 = filtered_depth_2024['magnitude'].mean()
         fig, ax = plt.subplots()
         ax.bar(['2023', '2024'], [avg_mag_2023, avg_mag_2024], color=['orange', 'green'])
         ax.set_title('Média de Magnitude por Ano (Filtrado por Profundidade)')
         ax.set_ylabel('Magnitude Média')
         st.pyplot(fig)
 
-        # Gráfico de pizza com contagem de terremotos por lugar
+        # Gráfico 3
         place_counts_2023 = filtered_depth_2023['place'].value_counts().head(10)  # Top 10 locais
         place_counts_2024 = filtered_depth_2024['place'].value_counts().head(10)  # Top 10 locais
 
-        # Gráfico de pizza para 2023
-        fig, ax = plt.subplots()
+        # Gráfico 4
+        fig, ax = plt.subplots(figsize=(10, 6))
         ax.pie(place_counts_2023, labels=place_counts_2023.index, autopct='%1.1f%%', startangle=90)
         ax.set_title('Distribuição de Terremotos por Lugar em 2023 (Filtrado por Profundidade)')
+        ax.legend(place_counts_2023.index, title='Top 10 Lugares', loc='center left', bbox_to_anchor=(1.5, 0.5))
         st.pyplot(fig)
 
-        # Gráfico de pizza para 2024
-        fig, ax = plt.subplots()
+        # Gráfico 5
+        fig, ax = plt.subplots(figsize=(10, 6))
         ax.pie(place_counts_2024, labels=place_counts_2024.index, autopct='%1.1f%%', startangle=90)
         ax.set_title('Distribuição de Terremotos por Lugar em 2024 (Filtrado por Profundidade)')
+        ax.legend(place_counts_2024.index, title='Top 10 Lugares', loc='center left', bbox_to_anchor=(1.5, 0.5))
         st.pyplot(fig)
 
 
